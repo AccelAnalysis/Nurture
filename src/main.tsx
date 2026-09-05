3e2361d243d183fc99ad0ba5151e6bd81ffd4e7c
AccelAnalysis <jholman@accelanalysis.com>
2026-09-05 07:07:44 -0400

    feat(track-f): install public analytics compatibility bridge

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { installLegacyPublicAnalyticsBridge } from "./features/analytics";
 import "./styles.css";
 import "./owner-contracts.css";
+
+installLegacyPublicAnalyticsBridge();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
