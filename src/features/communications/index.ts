c178ea2881e52285adfe0c72ea080b0241f15519
AccelAnalysis <jholman@accelanalysis.com>
2026-09-05 09:11:29 -0400

    feat(r2-d): expose communications feature boundary

---
+++ src/features/communications/index.ts
@@ -0,0 +1,4 @@
+export { CommunicationsAdminPage } from "./CommunicationsAdminPage";
+export { EmailPreferencesPage } from "./EmailPreferencesPage";
+export * as communicationsClient from "./client";
+export type * from "./contracts";
