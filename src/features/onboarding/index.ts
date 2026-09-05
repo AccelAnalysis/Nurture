80504ac63ce477f3b754a802b62259fb5c7dbdde
AccelAnalysis <jholman@accelanalysis.com>
2026-09-05 07:41:06 -0400

    feat(track-c): export Experience onboarding integration

--- src/features/onboarding/index.ts
+++ src/features/onboarding/index.ts
@@ -1,4 +1,16 @@
 export { defaultOnboardingDefinition, resolveOnboardingDefinition } from "./model/defaultDefinition";
+export {
+  createExperienceOnboardingBridge,
+  experienceRequirementsToOnboardingExtension,
+} from "./experienceBridge";
+export type {
+  ExperienceOnboardingBridge,
+  ExperienceOnboardingCompletionInput,
+  ExperienceOnboardingContext,
+  ExperienceOnboardingContextProvider,
+  ExperienceOnboardingRequirementLike,
+  ExperienceOnboardingResult,
+} from "./experienceBridge";
 export type {
   OnboardingAgreementDefinition,
   OnboardingDefinition,
