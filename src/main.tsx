b085e9be4a3bff0b2ccd6d2feee4787d8eaaa503
AccelAnalysis <jholman@accelanalysis.com>
2026-09-05 07:11:50 -0400

    refactor(track-f): avoid Track A main entry conflict

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { installLegacyPublicAnalyticsBridge } from "./features/analytics";
 import "./styles.css";
 import "./owner-contracts.css";
-
-installLegacyPublicAnalyticsBridge();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
